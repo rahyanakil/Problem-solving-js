2a801a5aaf51fa775aaa08b55d0ef55192088d54
rahyanakil <rahyanakil89@gmail.com>
2025-11-26 19:00:22 +0600

    updated with binary search

---
+++ problemsolve8(Binary Search Algo).js
@@ -0,0 +1,20 @@
+const binarySearch = (arr, target) => {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+  //   return undefined;
+};
+
+console.log(binarySearch([3, 5, 6, 7, 9, 11], 7));
+// output:3
